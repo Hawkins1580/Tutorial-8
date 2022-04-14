88b08eca504edcc7f87bc67fa4fcec2659dc1d95
hawkinsnick <nhawkins@hamilton.edu>
2022-04-14 00:28:48 +0000

    starting tutorial #8

---
+++ Tutorial#8.R
@@ -0,0 +1,15 @@
+
+# Start of Tutorial #8
+
+# Installing Packages
+install.packages(c("caret", "randomForest", "rgdal", "sf", "raster"))
+
+library(caret)
+library(randomForest)
+library(rgdal)
+library(sf)
+library(raster)
+
+
+
+
